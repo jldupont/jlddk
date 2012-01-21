4d2d25edbf14d34978d00d565d5e3a16e08a3c7a
jldupont <github@jldupont.com>
2012-01-21 09:37:47 -0500

    initial

---
+++ src/jlddk/block/mq_s3pub.py
@@ -0,0 +1,14 @@
+"""
+    Publishes a message through ZeroMQ
+    
+    Meant to be used in conjunction with, as example, jldaws / jlds3notify
+    
+    Created on 2012-01-20
+    @author: jldupont
+"""
+
+def run(bucket_name, prefix, keys={}, changes=[]):
+    """
+    
+    """
+    

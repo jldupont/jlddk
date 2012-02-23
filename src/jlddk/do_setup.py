03f24784d26ac544c140f4b85a21e2dbae170139
jldupont <github@jldupont.com>
2012-02-23 16:53:36 -0500

    0.1.24

--- src/jlddk/do_setup.py
+++ src/jlddk/do_setup.py
@@ -7,8 +7,10 @@ import logging,sys,os
 ## force stdout flushing
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
+name=os.path.basename(sys.argv[0])
+fname="%-10s" % name
 
-FORMAT='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+FORMAT='%(asctime)s - '+fname+' - %(levelname)s - %(message)s'
 formatter = logging.Formatter(FORMAT)
 
 logging.basicConfig(level=logging.INFO, format=FORMAT)

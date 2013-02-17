4ec696bebd22012a6101db9a3f1787b34990e78b
jldupont <github@jldupont.com>
2013-02-17 10:14:52 -0500

    0.7.30

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
     @author: jldupont
 """
 __author__  ="Jean-Lou Dupont"
-__version__ ="0.7.29"
+__version__ ="0.7.30"
 
 
 from distutils.core import setup
--- src/jlddk/script_clock.py
+++ src/jlddk/script_clock.py
@@ -14,6 +14,8 @@ def stdout(jo):
 def run(topic_name=None
         ,separate_msg_marker=False
         ,suppress_second_marker=False
+        ,seconds_count=None
+        ,tick_at_start=False
         ,**_
         ):
 
@@ -27,57 +29,80 @@ def run(topic_name=None
        ,"hour_marker": False
        ,"day_marker": False
     }
+    
 
     ppid=os.getppid()        
     logging.info("Process pid: %s" % os.getpid())
-    logging.info("Parent pid: %s" % ppid)
+    logging.info("Parent pid:  %s" % ppid)
     logging.info("Starting loop...")
+    
+    if tick_at_start:
+        send_tick()
+        
     
     while True:
         if os.getppid()!=ppid:
             logging.warning("Parent terminated... exiting")
             break
 
-        d["min_marker"]=False
-        d["hour_marker"]=False
-        d["day_marker"]=False
-        
-        sec=d["sec"]+1
-        d["min_marker"]=(sec==60)
-        d["sec"]=sec % 60
-        
-        if d["min_marker"]:
-            _min=d["min"]+1
-            d["hour_marker"]=(_min==60)
-            d["min"]=_min % 60
+        if seconds_count is None:
+            d=mode_default(d, suppress_second_marker, separate_msg_marker)
+        else:
+            mode_interval(seconds_count)
             
-            if d["hour_marker"]:            
-                hour=d["hour"]+1
-                d["day_marker"]=(hour==24)
-                d["hour"]=hour % 24
-                
-                if d["day_marker"]:                    
-                    d["day"]=d["day"]+1
+def mode_interval(interval_in_seconds):
+    
+    sleep(interval_in_seconds)
+    send_tick()
         
-        #logging.debug(" sec(%s) min_marker(%s) hour_marker(%s) day_marker(%s)" % (sec, min_marker, hour_marker, day_marker))
-        if suppress_second_marker:
-            if not d["min_marker"] and not d["hour_marker"] and not d["day_marker"]:
-                sleep(ONE_SECOND)
-                continue
-               
-        try:
-            stdout(d)
-            
-            if separate_msg_marker:
-                if d["min_marker"]:
-                    stdout({"topic":"min_marker"})
-                if d["hour_marker"]:
-                    stdout({"topic":"hour_marker"})
-                if d["day_marker"]:
-                    stdout({"topic":"day_marker"})
-        except:
-            raise Exception("Exiting... probably broken pipe")
-        
-        sleep(ONE_SECOND)
 
+def send_tick():
+    stdout({"topic":"tick"})
+
+
+def mode_default(d, suppress_second_marker, separate_msg_marker):
+
+    d["min_marker"]=False
+    d["hour_marker"]=False
+    d["day_marker"]=False
+    
+    sec=d["sec"]+1
+    d["min_marker"]=(sec==60)
+    d["sec"]=sec % 60
+    
+    if d["min_marker"]:
+        _min=d["min"]+1
+        d["hour_marker"]=(_min==60)
+        d["min"]=_min % 60
+        
+        if d["hour_marker"]:            
+            hour=d["hour"]+1
+            d["day_marker"]=(hour==24)
+            d["hour"]=hour % 24
+            
+            if d["day_marker"]:                    
+                d["day"]=d["day"]+1
+    
+    #logging.debug(" sec(%s) min_marker(%s) hour_marker(%s) day_marker(%s)" % (sec, min_marker, hour_marker, day_marker))
+    if suppress_second_marker:
+        if not d["min_marker"] and not d["hour_marker"] and not d["day_marker"]:
+            sleep(ONE_SECOND)
+            continue
+           
+    try:
+        stdout(d)
+        
+        if separate_msg_marker:
+            if d["min_marker"]:
+                stdout({"topic":"min_marker"})
+            if d["hour_marker"]:
+                stdout({"topic":"hour_marker"})
+            if d["day_marker"]:
+                stdout({"topic":"day_marker"})
+    except:
+        raise Exception("Exiting... probably broken pipe")
+    
+    sleep(ONE_SECOND)
+
+    return d
 
